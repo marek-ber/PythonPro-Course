63fbe9718df66d4c2078304ac9e81ce3dd46ad03
Marek Berny <marek.berny@gmail.com>
2026-04-03 21:46:13 +0200

    task2

--- homework/lesson1/task2_greeting.py
+++ homework/lesson1/task2_greeting.py
@@ -6,4 +6,4 @@
 first_name = input("Jak masz na imię ?: ")
 age = input("Ile masz lat ?: ")
 
-print("Cześć,", first_name, "!", "Wiem, że masz", age, "lat")
+print("Cześć,", first_name, "!", "Wiem, że masz", age, "lat.")
